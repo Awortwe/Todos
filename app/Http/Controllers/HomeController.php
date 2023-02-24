5af57e0fab03f1ff72a759bf928f0e524d95c6ac
Awortwe <enockawor@gmail.com>
2023-02-24 05:48:16 -0800

    Working on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tasks = Task::where('user_id', auth()->id())->get();
+        return view('home', compact('tasks'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}
+                    <span style="float: right"><a href="" class="btn btn-secondary btn-sm">Create Task</a></span>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +16,22 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @if (count($tasks))
+                       <table class="table table-striped">
+                            <tr>
+                                <th>Tasks</th>
+                                <th>Description</th>
+                            </tr>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td>{{ $task->title }}</td>
+                                    <td>{{ $task->description }}</td>
+                                </tr>
+                            @endforeach
+                       </table>
+                    @else
+                       <p>You don't have any tasks yet!!</p>
+                    @endif
                 </div>
             </div>
         </div>
